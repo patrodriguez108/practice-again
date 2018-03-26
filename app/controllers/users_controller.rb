f78626c96ed019cac1892fca0a6d7c50d50a5f17
Patrick <pat.rodriguez108@gmail.com>
2018-03-26 14:39:37 -0500

    Enable user registration via params and create path

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,14 @@ class UsersController < ApplicationController
 
 	def new
 	end
+
+	def create
+		p @user = User.new(user_params)
+	end
+
+	private
+
+	def user_params
+		params.require(:user).permit(:first_name, :last_name, :username, :email, :password, :password_confirmation)
+	end
 end
