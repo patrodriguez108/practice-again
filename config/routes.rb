82374962c5b8e4d9beda68d504d991a8f4029e1f
Patrick <pat.rodriguez108@gmail.com>
2018-03-26 15:03:16 -0500

    Add routes for login/logout

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   get '/users/new', to: 'users#new'
   post '/users', to: 'users#create'
+
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  get '/logout', to: 'sessions#destroy'
 end
