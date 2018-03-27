53dd7108213f7cadb1acb86518198e174d346d45
Patrick <pat.rodriguez108@gmail.com>
2018-03-27 11:39:14 -0500

    Add functionality for posts index

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,8 +1,25 @@
 class PostsController < ApplicationController
 
 	def index
+		@posts = Post.all.order("DESC")
 	end
 
 	def new
+	end
+
+	def create
+		post = Post.new(post_params)
+		post.user_id = session[:user_id]
+		if post.save
+			redirect_to '/posts'
+		else
+			render 'new'
+		end
+	end
+
+	private
+
+	def post_params
+		params.require(:post).permit(:title, :content)
 	end
 end
