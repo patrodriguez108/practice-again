531dcabeaa19d858c0510cbb429e275ee7971c8d
Patrick <pat.rodriguez108@gmail.com>
2018-03-27 10:36:50 -0500

    Refactor users/new to signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  get '/users/new', to: 'users#new'
-  post '/users', to: 'users#create'
+  get '/signup', to: 'users#new'
+  post '/signup', to: 'users#create'
 
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
